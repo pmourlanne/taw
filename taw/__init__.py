aed2137d24955723346449f66e3f6e0fc1953a62
Pierre Mourlanne <pmourlanne@gmail.com>
2023-04-29 15:29:02 +0200

    Run pre-commit hooks on the repo

--- taw/__init__.py
+++ taw/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(
     __name__,
-    static_url_path='', 
-    static_folder='static',
-    template_folder='templates',
+    static_url_path="",
+    static_folder="static",
+    template_folder="templates",
 )
+
 
 @app.route("/")
 def hello_world():
