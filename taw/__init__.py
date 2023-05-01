9384cef5b8188b0b61c0e18a38067af00b49474a
Pierre Mourlanne <pmourlanne@gmail.com>
2023-05-01 15:41:21 +0200

    I fucked up a merge conflict, fixing now

--- taw/__init__.py
+++ taw/__init__.py
@@ -50,24 +50,6 @@ def home():
 
             return render_template("pairings.html", rows=rows, **ctx)
 
-        elif request.form["action"] == "match_slips":
-            pairings = form.parsed_pairings
-
-            rows = []
-            for pairing in pairings:
-                # We want to display each pairing twice, once for each player
-                rows.append(
-                    {
-                        "table_number": pairing.number,
-                        "player_1": pairing.player_1.name,
-                        "player_1_points": pairing.player_1.points,
-                        "player_2": pairing.player_2.name,
-                        "player_2_points": pairing.player_2.points,
-                    },
-                )
-            rows = sorted(rows, key=lambda row: row["player_1"].lower())
-            return render_template("pairings.html", rows=rows, **ctx)
-
         if request.form["action"] == "match_slips":
             pairings = form.parsed_pairings
             # We want to print five match slips per page, and we want
